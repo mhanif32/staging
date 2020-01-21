d5cd80719d972f9fcf0a83d9a706e92ec2a46314
Yogesh <yogeshphp.alkurn@gmail.com>
2020-01-21 13:28:21 +0530

    footer and logo chnages

--- admin/language/en-gb/common/footer.php
+++ admin/language/en-gb/common/footer.php
@@ -1,4 +1,4 @@
 <?php
 // Text
-$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
+$_['text_footer']  = '<a href="http://champion.alkurnwork.co.in/">TheChampionMall</a> &copy; 2019-' . date('Y') . ' All Rights Reserved.';
 $_['text_version'] = 'Version %s';
