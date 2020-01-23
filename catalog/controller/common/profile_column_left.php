bdb89488066509ce4df4b130e5fdc8b38a920cca
Yogesh <yogeshphp.alkurn@gmail.com>
2020-01-23 15:13:19 +0530

    edit profile

--- catalog/controller/common/profile_column_left.php
+++ catalog/controller/common/profile_column_left.php
@@ -2,10 +2,18 @@
 class ControllerCommonProfileColumnLeft extends Controller {
     public function index() {
 
+        $customer_info = [];
+        if ($this->request->server['REQUEST_METHOD'] != 'POST') {
+            $customer_info = $this->model_account_customer->getCustomer($this->customer->getId());
+            //print_r($customer_info['firstname']);exit('aaa');
+        }
+
+
         $data = [];
         $data['edit'] = $this->url->link('account/edit', '', true);
         $data['password'] = $this->url->link('account/password', '', true);
         $data['address'] = $this->url->link('account/address', '', true);
+        $data = array_merge($data, $customer_info);
         return $this->load->view('common/profile_column_left', $data);
     }
 }
