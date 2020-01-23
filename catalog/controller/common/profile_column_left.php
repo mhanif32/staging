a274c27bce116e4f607c797f17efecc052329971
Yogesh <yogeshphp.alkurn@gmail.com>
2020-01-23 16:34:07 +0530

    edit profile

--- catalog/controller/common/profile_column_left.php
+++ catalog/controller/common/profile_column_left.php
@@ -5,15 +5,19 @@ class ControllerCommonProfileColumnLeft extends Controller {
         $customer_info = [];
         if ($this->request->server['REQUEST_METHOD'] != 'POST') {
             $customer_info = $this->model_account_customer->getCustomer($this->customer->getId());
-            //print_r($customer_info['firstname']);exit('aaa');
         }
 
-
         $data = [];
+        $data['account'] = $this->url->link('account/account', '', true);
         $data['edit'] = $this->url->link('account/edit', '', true);
         $data['password'] = $this->url->link('account/password', '', true);
         $data['address'] = $this->url->link('account/address', '', true);
         $data = array_merge($data, $customer_info);
+        //print_r($customer_info);exit('asd');
+
+        $file = !empty($customer_info['image']) ? $customer_info['image'] : 'no-avatar.png';
+        $data['image_url'] = '/storage/upload/' . $file;
+        //echo $data['image_url'];exit('aaaa');
         return $this->load->view('common/profile_column_left', $data);
     }
 }
--- catalog/model/account/customer.php
+++ catalog/model/account/customer.php
@@ -23,11 +23,14 @@ class ModelAccountCustomer extends Model {
 	}
 
 	public function editCustomer($customer_id, $data) {
+
 		$this->db->query("UPDATE " . DB_PREFIX . "customer SET firstname = '" . $this->db->escape($data['firstname']) . "', lastname = '" . $this->db->escape($data['lastname']) . "', email = '" . $this->db->escape($data['email']) . "', telephone = '" . $this->db->escape($data['telephone']) . "' custom_field = '" . $this->db->escape(isset($data['custom_field']['account']) ? json_encode($data['custom_field']['account']) : '') . "' WHERE customer_id = '" . (int)$customer_id . "'");
+		return true;
 	}
 
 	public function editAvatar($customer_id, $file, $code) {
         $this->db->query("UPDATE " . DB_PREFIX . "customer SET image = '".$file."', code = '".$code."' WHERE customer_id = '" . (int)$customer_id . "'");
+        return true;
     }
 
 	public function editPassword($email, $password) {
