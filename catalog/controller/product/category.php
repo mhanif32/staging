e1dee39074378716da7050898188f594e2f077fa
Yogesh <yogeshphp.alkurn@gmail.com>
2020-02-18 15:25:41 +0530

    category list

--- catalog/controller/product/category.php
+++ catalog/controller/product/category.php
@@ -413,4 +413,28 @@ class ControllerProductCategory extends Controller
             $this->response->setOutput($this->load->view('error/not_found', $data));
         }
     }
+
+    public function listing()
+    {
+        $this->load->language('product/category');
+
+        $this->load->model('catalog/category');
+        $this->load->model('tool/image');
+
+        $data['categories'] = array();
+        $categories = $this->model_catalog_category->getCategories(0);
+        foreach ($categories as $category) {
+            $data['categories'][] = array(
+                'category_id' => $category['category_id'],
+                'name'        => $category['name'],
+                'image'        => $this->model_tool_image->resize($category['image'], 100, 100),
+                'href'        => $this->url->link('product/category', 'path=' . $category['category_id'])
+            );
+        }
+
+        $data['footer'] = $this->load->controller('common/footer');
+        $data['header'] = $this->load->controller('common/header');
+
+        $this->response->setOutput($this->load->view('product/category_listing', $data));
+    }
 }
