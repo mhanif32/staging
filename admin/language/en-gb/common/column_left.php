27bea9bf08fbdc3aca645f0a96fceb04083e6e32
Yogesh <yogeshphp.alkurn@gmail.com>
2020-03-31 12:21:13 +0530

    just commit

--- admin/language/en-gb/common/column_left.php
+++ admin/language/en-gb/common/column_left.php
@@ -70,6 +70,7 @@ $_['text_voucher_theme']        = 'Voucher Themes';
 $_['text_weight_class']         = 'Weight Classes';
 $_['text_length_class']         = 'Length Classes';
 $_['text_zone']                 = 'Zones';
+$_['text_area']                 = 'Areas';
 $_['text_recurring']            = 'Recurring Profiles';
 $_['text_order_recurring']      = 'Recurring Orders';
 $_['text_openbay_extension']    = 'OpenBay Pro';
