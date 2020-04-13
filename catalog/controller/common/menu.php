5b32abd8f9224378eeff2f42d3405d54c4ab8144
Alkurn Tech <tech.alkurn@gmail.com>
2020-04-13 23:01:18 +0530

    menu iisue resolved

--- catalog/controller/common/menu.php
+++ catalog/controller/common/menu.php
@@ -1,84 +1,87 @@
 <?php
-class ControllerCommonMenu extends Controller {
-	public function index() {
-		$this->load->language('common/menu');
 
-		// Menu
-		$this->load->model('catalog/category');
+class ControllerCommonMenu extends Controller
+{
+    public function index()
+    {
+        $this->load->language('common/menu');
 
-		$this->load->model('catalog/product');
+        // Menu
+        $this->load->model('catalog/category');
 
-		$data['categories'] = array();
+        $this->load->model('catalog/product');
 
-		$categories = $this->model_catalog_category->getCategories(0);
+        $data['categories'] = array();
 
-		foreach ($categories as $category) {
+        $categories = $this->model_catalog_category->getCategories(0);
 
-			if ($category['top']) {
-				// Level 2
-				$children_data = array();
+        foreach ($categories as $category) {
 
-				$children = $this->model_catalog_category->getCategories($category['category_id']);
+            if ($category['top']) {
+                // Level 2
+                $children_data = array();
+
+                $children = $this->model_catalog_category->getCategories($category['category_id']);
                 $data['subCategories'] = [];
-				foreach ($children as $child) {
-					$filter_data = array(
-						'filter_category_id'  => $child['category_id'],
-						'filter_sub_category' => true
-					);
+                foreach ($children as $child) {
+                    $filter_data = array(
+                        'filter_category_id' => $child['category_id'],
+                        'filter_sub_category' => true
+                    );
 
-                    //$subCategories = $this->model_catalog_category->getMoreCategories($child['category_id']);
                     $subCategories = $this->model_catalog_category->getCategories($child['category_id']);
-                    foreach($subCategories as $subCategory) {
-                        $data['subCategories'][] = array(
+                    $subCategoriesArray = array();
+                    foreach ($subCategories as $subCategory) {
+                        $subCategoriesArray[] = array(
                             'category_id' => $subCategory['category_id'],
-                            'name'        => $subCategory['name'],
-                            'href'        => $this->url->link('product/category', 'path=' . $subCategory['category_id'])
+                            'name' => $subCategory['name'],
+                            'href' => $this->url->link('product/category', 'path=' . $subCategory['category_id'])
                         );
                     }
-					$children_data[] = array(
-						//'name'  => $child['name'] . ($this->config->get('config_product_count') ? ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')' : ''),
-                        'name'  => $child['name'],
-						'href'  => $this->url->link('product/category', 'path=' . $category['category_id'] . '_' . $child['category_id']),
-                        'subCategories' =>  !empty($data['subCategories']) ? $data['subCategories'] : []
-					);
-				}
+                    $children_data[] = array(
+                        //'name'  => $child['name'] . ($this->config->get('config_product_count') ? ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')' : ''),
+                        'name' => $child['name'],
+                        'href' => $this->url->link('product/category', 'path=' . $category['category_id'] . '_' . $child['category_id']),
+                        'subCategories' => !empty($subCategoriesArray) ? $subCategoriesArray : []
+                    );
+                }
 
-				// Level 1
-				$data['categories'][] = array(
-					'name'     => $category['name'],
-					'children' => $children_data,
-					'column'   => $category['column'] ? $category['column'] : 1,
-					'href'     => $this->url->link('product/category', 'path=' . $category['category_id'])
-				);
-				$data['sale_offer_link'] = $this->url->link('product/special');
-				$data['more_category'] = $this->url->link('product/category/listing');
-			}
-		}
+                // Level 1
+                $data['categories'][] = array(
+                    'name' => $category['name'],
+                    'children' => $children_data,
+                    'column' => $category['column'] ? $category['column'] : 1,
+                    'href' => $this->url->link('product/category', 'path=' . $category['category_id'])
+                );
+                $data['sale_offer_link'] = $this->url->link('product/special');
+                $data['more_category'] = $this->url->link('product/category/listing');
+            }
+        }
 
         //more categories
         $this->load->model('catalog/category');
         $data['moreCategories'] = array();
         $categories = $this->model_catalog_category->getMoreCategories(0);
-        foreach($categories as $category) {
+        foreach ($categories as $category) {
 
             $data['subCategories'] = array();
             $subCategories = $this->model_catalog_category->getMoreCategories($category['category_id']);
-            foreach($subCategories as $subCategory) {
+            foreach ($subCategories as $subCategory) {
                 $data['subCategories'][] = array(
                     'category_id' => $subCategory['category_id'],
-                    'name'        => $subCategory['name'],
-                    'href'        => $this->url->link('product/category', 'path=' . $subCategory['category_id'])
+                    'name' => $subCategory['name'],
+                    'href' => $this->url->link('product/category', 'path=' . $subCategory['category_id'])
                 );
             }
 
             $data['moreCategories'][] = array(
                 'category_id' => $category['category_id'],
-                'name'        => $category['name'],
-                'href'        => $this->url->link('product/category', 'path=' . $category['category_id']),
-                'subCategories' =>  $data['subCategories']
+                'name' => $category['name'],
+                'href' => $this->url->link('product/category', 'path=' . $category['category_id']),
+                'subCategories' => $data['subCategories']
             );
         }
         //echo '<pre>'; print_r($data); exit('okok');
-		return $this->load->view('common/menu', $data);
-	}
+        return $this->load->view('common/menu', $data);
+    }
 }
