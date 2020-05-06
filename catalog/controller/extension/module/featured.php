f6b2194aa19122b5bdc98f562dea732ab5d07c92
Alkurn Tech <yogeshphp.alkurn@gmail.com>
2020-05-06 13:14:57 +0530

    home page & footer links

--- catalog/controller/extension/module/featured.php
+++ catalog/controller/extension/module/featured.php
@@ -2,7 +2,6 @@
 class ControllerExtensionModuleFeatured extends Controller {
 	public function index($setting) {
 
-	    //print_r($setting);exit('ook');
 		$this->load->language('extension/module/featured');
 
 		$this->load->model('catalog/product');
@@ -70,47 +69,6 @@ class ControllerExtensionModuleFeatured extends Controller {
         $data['count_retailers'] = $this->model_catalog_product->getRetailersCount();
         $data['sellers_href'] = $this->url->link('mpmultivendor/mv_seller', '', true);
 
-		//men count and path
-        $countMen = $this->model_catalog_product->getCountMen();
-        $data['count_men'] = $countMen['total'];
-
-        $categoryMen = $this->model_catalog_product->getCategoryMen();
-        $data['path_men'] = !empty($categoryMen['total']) ? $this->url->link('product/category', 'path=' . $categoryMen['path']) : '#';
-        if ($categoryMen['image']) {
-            $data['men_thumb'] = $this->model_tool_image->resize($categoryMen['image'], 250, 177);
-        } else {
-            $data['men_thumb'] = $this->model_tool_image->resize('placeholder.png', 250, 177);;
-        }
-
-        //women count and path
-        $countWomen = $this->model_catalog_product->getCountWomen();
-        $data['count_women'] = $countWomen['total'];
-
-        $categoryWomen = $this->model_catalog_product->getCategoryWomen();
-        $data['path_women'] = !empty($categoryWomen['total']) ? $this->url->link('product/category', 'path=' . $categoryWomen['path']) : '#';
-        if ($categoryWomen['image']) {
-            $data['women_thumb'] = $this->model_tool_image->resize($categoryWomen['image'], 250, 177);
-        } else {
-            $data['women_thumb'] = $this->model_tool_image->resize('placeholder.png', 250, 177);;
-        }
-
-        //brand category and path
-        $categoryDesignerBrands = $this->model_catalog_product->getCountDesignerBrands();
-        $data['count_brands'] = $categoryDesignerBrands['total'];
-        $data['path_brands'] = !empty($categoryDesignerBrands['total']) ? $this->url->link('product/category', 'path=' . $categoryDesignerBrands['path']) : '#';
-        if ($categoryDesignerBrands['image']) {
-            $data['brand_thumb'] = $this->model_tool_image->resize($categoryDesignerBrands['image'], 250, 177);
-        } else {
-            $data['brand_thumb'] = $this->model_tool_image->resize('placeholder.png', 250, 177);;
-        }
-
-        $this->load->model('design/banner');
-        $retailerBanner = $this->model_design_banner->getBannerSingle(9);
-        $data['RetailerBanners'] = array(
-            'title' => $retailerBanner['title'],
-            'link'  => $retailerBanner['link'],
-            'image' => $this->model_tool_image->resize($retailerBanner['image'], 250, 177)
-        );
 		if ($data['products']) {
 			return $this->load->view('extension/module/featured', $data);
 		}
