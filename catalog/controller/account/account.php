51af1f7d534a275112e84400668b602a6222ba73
Alkurn Tech <yogeshphp.alkurn@gmail.com>
2020-05-15 12:07:39 +0530

    state & country wise functionality

--- catalog/controller/account/account.php
+++ catalog/controller/account/account.php
@@ -209,8 +209,8 @@ class ControllerAccountAccount extends Controller {
 
     public function addDeliveryInfo()
     {
-        error_reporting(E_ALL);
-        ini_set("display_errors", 1);
+//        error_reporting(E_ALL);
+//        ini_set("display_errors", 1);
 
         $this->load->language('account/delivery_partner');
 
