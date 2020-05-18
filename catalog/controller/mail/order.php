e8209ffc31f6eb6c37e0260a771a07d2295f5e90
Alkurn Tech <yogeshphp.alkurn@gmail.com>
2020-05-18 19:44:32 +0530

    issue resolve

--- catalog/controller/mail/order.php
+++ catalog/controller/mail/order.php
@@ -468,7 +468,10 @@ class ControllerMailOrder extends Controller {
 			$mail->setFrom($this->config->get('config_email'));
 			$mail->setSender(html_entity_decode($order_info['store_name'], ENT_QUOTES, 'UTF-8'));
 			$mail->setSubject(html_entity_decode(sprintf($this->language->get('text_subject'), $this->config->get('config_name'), $order_info['order_id']), ENT_QUOTES, 'UTF-8'));
-            $mailText = $this->load->view($this->load->view('mail/order_alert', $data));
+//			$mail->setText($this->load->view('mail/order_alert', $data));
+//			$mail->send();
+
+            $mailText = $this->load->view('mail/order_alert', $data);
             $mail->setHtml($mailText);
             $mail->setText(html_entity_decode($mailText, ENT_QUOTES, 'UTF-8'));
             $mail->send();
