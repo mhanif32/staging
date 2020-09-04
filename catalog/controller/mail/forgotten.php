a5a5136b636ebe1b762a595a421f9b06d459e1e9
Alkurn Tech <yogeshphp.alkurn@gmail.com>
2020-09-04 23:57:29 +0530

    forgot mail

--- catalog/controller/mail/forgotten.php
+++ catalog/controller/mail/forgotten.php
@@ -2,6 +2,7 @@
 class ControllerMailForgotten extends Controller {
 	public function index(&$route, &$args, &$output) {			            
 		$this->load->language('mail/forgotten');
+        $this->load->model('account/customer');
 
 		$data['text_greeting'] = sprintf($this->language->get('text_greeting'), html_entity_decode($this->config->get('config_name'), ENT_QUOTES, 'UTF-8'));
 		$data['text_change'] = $this->language->get('text_change');
@@ -23,6 +24,14 @@ class ControllerMailForgotten extends Controller {
 		$mail->setSender(html_entity_decode($this->config->get('config_name'), ENT_QUOTES, 'UTF-8'));
 		$mail->setSubject(html_entity_decode(sprintf($this->language->get('text_subject'), html_entity_decode($this->config->get('config_name'), ENT_QUOTES, 'UTF-8')), ENT_QUOTES, 'UTF-8'));
 
+        if ($this->request->server['HTTPS']) {
+            $server = $this->config->get('config_ssl');
+        } else {
+            $server = $this->config->get('config_url');
+        }
+        $data['logo'] = $server . 'image/' . $this->config->get('config_logo');
+        $customer = $this->model_account_customer->getCustomerByEmail($args[0]);
+        $data['text_firstname'] = $customer['firstname'];
         $mailText = $this->load->view('mail/forgotten', $data);
         $mail->setHtml($mailText);
         $mail->setText(html_entity_decode($mailText, ENT_QUOTES, 'UTF-8'));
