902f7555500afe3f4979eb3072fef59ab6515555
Your Name <smitil.alkurn@gmail.com>
2020-09-11 11:30:22 +0530

    testing pages and fixed

--- catalog/controller/information/steps_upload_product_info.php
+++ catalog/controller/information/steps_upload_product_info.php
@@ -4,11 +4,25 @@ class ControllerInformationStepsUploadProductInfo extends Controller {
         // Optional. This calls for your language file
         $this->load->language('information/delivery_info');
 
+        $data['breadcrumbs'] = array();
+
+        $data['breadcrumbs'][] = array(
+            'text' => $this->language->get('text_home'),
+            'href' => $this->url->link('common/home')
+        );
+
+        $data['breadcrumbs'][] = array(
+            'text' => $this->language->get('Account'),
+            'href' => $this->url->link('account/account', '', true)
+        );
+
+
         // Optional. Set the title of your web page
         $this->document->setTitle($this->language->get('heading_title'));
 
         // Get "heading_title" from language file
         $data['heading_title'] = $this->language->get('heading_title');
+
 
         // All the necessary page elements
         $data['column_left'] = $this->load->controller('common/column_left');
