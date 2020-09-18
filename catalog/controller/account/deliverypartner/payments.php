d4e7300938703222418c88d2b6795264e57f3bda
Alkurn Tech <yogeshphp.alkurn@gmail.com>
2020-09-18 22:50:21 +0530

    delivery charge

--- catalog/controller/account/deliverypartner/payments.php
+++ catalog/controller/account/deliverypartner/payments.php
@@ -4,45 +4,56 @@ class ControllerAccountDeliverypartnerPayments extends Controller
 {
     public function index()
     {
-        $from = "kathmandu,nepal";
-        $to = "mahendranagar,nepal";
-        $from = urlencode($from);
-        $to = urlencode($to);
-        $data = file_get_contents("http://maps.googleapis.com/maps/api/distancematrix/json?origins=$from&destinations=$to&language=en-EN&sensor=false");
-        $data = json_decode($data);
-        $time = 0;
-        $distance = 0;
-        echo '<pre>';print_r($data->rows);exit('okokok');
-        foreach($data->rows[0]->elements as $road) {
-            $time += $road->duration->value;
-            $distance += $road->distance->value;
-        }
-        echo "To: ".$data->destination_addresses[0];
-        echo "<br/>";
-        echo "From: ".$data->origin_addresses[0];
-        echo "<br/>";
-        $distance=$distance/1000;
-        echo "Distance: ".$distance." km";
-        $default_price=11;
-        $distance_mile=$distance*1.6;
-        $price_distance=$default_price*$distance_mile;
 
-        if($distance_mile>=25){
-            echo "<script>alert('We are sorry. Your address falls outside of our service area and we are unable to deliver/pick up equipment to you at this time.');</script>";
+//Our starting point / origin. Change this if you wish.
+        $start = "Cork, Ireland";
+
+//Our end point / destination. Change this if you wish.
+        $destination = "Dublin, Ireland";
+
+//The Google Directions API URL. Do not change this.
+        $apiUrl = 'http://maps.googleapis.com/maps/api/directions/json';
+
+//Construct the URL that we will visit with cURL.
+        $url = $apiUrl . '?' . 'origin=' . urlencode($start) . '&destination=' . urlencode($destination);
+
+//Initiate cURL.
+        $curl = curl_init($url);
+
+//Tell cURL that we want to return the data.
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+//Execute the request.
+        $res = curl_exec($curl);
+
+//If something went wrong with the request.
+        if(curl_errno($curl)){
+            throw new Exception(curl_error($curl));
         }
-        elseif($price_distance>=500){
-            $price=$price_distance;
-        }else{
-            if($distance_mile<5){
-                $price=$price_distance;
-            }elseif($distance_mile>5 && $distance_mile<10){
-                $price=$default_price + 20;
-            }
-            elseif($distance_mile>10 && $distance_mile<25){
-                $price=$default_price + 30;
-            }
+
+//Close the cURL handle.
+        curl_close($curl);
+
+//Decode the JSON data we received.
+        $json = json_decode(trim($res), true);
+
+//Automatically select the first route that Google gave us.
+        $route = $json['routes'][0];
+
+//Loop through the "legs" in our route and add up the distances.
+        $totalDistance = 0;
+        foreach($route['legs'] as $leg){
+            $totalDistance = $totalDistance + $leg['distance']['value'];
         }
-        echo "final price : ".$price;exit('okokok');
+
+//Divide by 1000 to get the distance in KM.
+        $totalDistance = round($totalDistance / 1000);
+
+//Print out the result.
+        echo 'Total distance is ' . $totalDistance . 'km';
+
+//var_dump the original array, for illustrative purposes.
+        var_dump($json);exit('okokok');
 
         error_reporting(E_ALL);
         ini_set("display_errors", 1);
